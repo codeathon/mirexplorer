8364dff8f5ce1d33ac012420daac41f58080781d
huwcheston <hwc31@cam.ac.uk>
2026-01-05 15:24:34 +0000

    load env variables on app start

--- app.py
+++ app.py
@@ -4,12 +4,18 @@
 MIRExplorer main app
 """
 
+from dotenv import load_dotenv, find_dotenv
+
 from backend import FLASK_APP, get_scheduler, app_data
 
 DEVELOPMENT_ENV = True
 
 if __name__ == "__main__":
+    # load env variables
+    load_dotenv(find_dotenv())
+
     # Start the task scheduler (runs in same process as app)
     get_scheduler().start()
+
     # Start the app
     FLASK_APP.run(debug=DEVELOPMENT_ENV)
