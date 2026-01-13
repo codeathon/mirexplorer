fc63a269ac53d21db2143f524372410c68f5b4b2
huwcheston <hwc31@cam.ac.uk>
2026-01-13 11:47:51 +0000

    disable rate limiter in dev mode

--- app.py
+++ app.py
@@ -4,18 +4,25 @@
 MIRExplorer main app
 """
 
+import os
+
 from dotenv import load_dotenv, find_dotenv
 
 from backend import FLASK_APP, get_scheduler, app_data
-
-DEVELOPMENT_ENV = True
+from backend.extensions import limiter
 
 if __name__ == "__main__":
     # load env variables
     load_dotenv(find_dotenv())
 
+    DEVELOPMENT_ENV = os.getenv("DEVELOPMENT_ENV") == "true"
+
     # Start the task scheduler (runs in same process as app)
     get_scheduler().start()
+
+    # Disable the limiter if running in a development environment
+    if DEVELOPMENT_ENV:
+        limiter.enabled = False
 
     # Start the app
     FLASK_APP.run(debug=DEVELOPMENT_ENV)
