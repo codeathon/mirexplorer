97cdfd91f7fae2b0533acf8290c6c9382d867ce6
huwcheston <hwc31@cam.ac.uk>
2026-01-20 16:22:59 +0000

    try fix build again

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 MIRExplorer main app
 """
 
+import json
 import os
 
 from dotenv import load_dotenv, find_dotenv
@@ -12,8 +13,17 @@ from backend import FLASK_APP, get_scheduler, app_data
 from backend.extensions import limiter
 
 
+def vite_asset(entry):
+    manifest_path = FLASK_APP.static_folder + "/dist/.vite/manifest.json"
+    with open(manifest_path) as f:
+        manifest = json.load(f)
+    return manifest[entry]
+
+
 # make gunicorn happy :)
 app = FLASK_APP
+app.jinja_env.globals["vite_asset"] = vite_asset
+
 
 if __name__ == "__main__":
     # load env variables
